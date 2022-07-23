e3f8134780579ad0531877cd344bbb975eb08a0b
AfonsoBertoncini <bertoncinis@hotmail.com>
2022-07-23 16:24:56 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/afonso/bookstore/domain/Livro.java
@@ -0,0 +1,19 @@
+package com.afonso.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Livro {
+
+	private Integer id;
+	private String nome_autor, texto, titulo;
+	private Categoria categoria;
+	
+	
+	
+	
+	
+	
+	
+}
