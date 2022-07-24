cb5223d4758114043e8a3305234aca1137ed23e1
AfonsoBertoncini <bertoncinis@hotmail.com>
2022-07-24 17:22:09 -0300

    commit teste

--- src/main/java/com/afonso/bookstore/domain/Livro.java
+++ src/main/java/com/afonso/bookstore/domain/Livro.java
@@ -1,19 +1,90 @@
 package com.afonso.bookstore.domain;
 
+import java.io.Serializable;
+
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+
+@Entity
+
+public class Livro implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome_autor, texto, titulo;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
+
 	
-	
-	
-	
-	
-	
-	
+	public Livro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Livro(Integer id, String nome_autor, String texto, String titulo, Categoria categoria) {
+		super();
+		this.id = id;
+		this.nome_autor = nome_autor;
+		this.texto = texto;
+		this.titulo = titulo;
+		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome_autor() {
+		return nome_autor;
+	}
+
+	public void setNome_autor(String nome_autor) {
+		this.nome_autor = nome_autor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
 }
